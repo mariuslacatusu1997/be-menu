8ac9f7032690527a169f679b524977e52ca9607b
CWS\marius.lacatusu <marius.lacatusu@cws.it>
2024-11-05 19:36:05 +0100

    redmi add

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const corsOptions = {
 
 app.use(cors(corsOptions)); // Use the CORS middleware
 app.use(express.json());
-
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
 const categoriesFilePath = path.join(__dirname, '../backend/src/categories.xlsx');
 const productsFilePath = path.join(__dirname, '../backend/src/products.xlsx');
 
